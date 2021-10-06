292f369f66041500a6dd81ea4d314034142e9290
UnderPaidMathematician <61880854+UnderPaidMathematician@users.noreply.github.com>
2021-10-06 08:24:14 -0400

    Day 1 Complete

---
+++ 100 Days of Code/1_day_p5.py
@@ -0,0 +1,16 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+
+print("Welcome to the band name generator.\n\n")
+home_town = input("Please enter the name of your home town:\n")
+favorite_pet = input("Please enter the name of your favorite pet: \n")
+print(f"\nYour band name should be \'{home_town} {favorite_pet}\'!")
+
+
